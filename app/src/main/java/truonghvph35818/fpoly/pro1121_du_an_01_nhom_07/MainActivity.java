a44b7dd59750d726e5fdb093ed0659091d6340ed
Admin <truonghv2k4@gmail.com>
2023-11-01 23:05:55 +0700

    coment

---
+++ app/src/main/java/truonghvph35818/fpoly/pro1121_du_an_01_nhom_07/MainActivity.java
@@ -0,0 +1,21 @@
+package truonghvph35818.fpoly.pro1121_du_an_01_nhom_07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 1200);
+    }
+}
