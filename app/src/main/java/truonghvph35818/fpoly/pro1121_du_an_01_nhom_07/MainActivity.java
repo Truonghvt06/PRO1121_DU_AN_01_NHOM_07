10dc0bee7dffa6e4284a0fe6cf0428ed31b2c94d
Luong The Tai <tailtph35654@fpt.edu.vn>
2023-11-13 16:01:59 +0700

    tai

--- app/src/main/java/truonghvph35818/fpoly/pro1121_du_an_01_nhom_07/MainActivity.java
+++ app/src/main/java/truonghvph35818/fpoly/pro1121_du_an_01_nhom_07/MainActivity.java
@@ -7,15 +7,5 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
 
-            }
-        }, 1200);
-    }
 }
